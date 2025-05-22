4be75a41610822443727f6910c86c3840a369607
bantee-sharma <banteesharma9@gmail.com>
2025-05-22 23:15:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from langchain_community.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
 loader = PyMuPDFLoader("SQL Revision Notes.pdf")
 
 document = loader.load()
@@ -17,8 +25,24 @@ vector_store = FAISS.from_documents(chunks,embeddings)
 
 retriever = vector_store.as_retriever(search_type="similarity",kwargs={"k":3})
 
+prompt = PromptTemplate(
+    template='''You are a helpfull AI Assistant.
+    Answer the question from the following context. If Context if insufficient just say, I don't know.
+    {context}
+    Question:{question}
+    Answer:''',
+    input_variables=["context","question"]
+)
+
+
+
 question = "what is sql"
 retrieve_docs = retriever.invoke(question)
 
 context = " ".join([i.page_content for i in retrieve_docs])
-print(context)
+
+final_prompt = prompt.invoke({"context":context,"question":question})
+
+result = llm.invoke(final_prompt)
+print(result.content)
+
