79165662d91aaecd6267e8ad514a61928f2cc980
bantee-sharma <banteesharma9@gmail.com>
2025-05-23 21:06:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,30 @@
-from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_community.document_loaders import TextLoader,PyMuPDFLoader,UnstructuredWordDocumentLoader
+from pathlib import Path
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
-from langchain_core.prompts import PromptTemplate
-from langchain_google_genai import ChatGoogleGenerativeAI
-from dotenv import load_dotenv
-
-load_dotenv()
-
-llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
-
-loader = PyMuPDFLoader("SQL Revision Notes.pdf")
-
-document = loader.load()
-
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=100
-)
-
-chunks = text_splitter.split_documents(document)
-embeddings = HuggingFaceEmbeddings()
-
-vector_store = FAISS.from_documents(chunks,embeddings)
-
-retriever = vector_store.as_retriever(search_type="similarity",kwargs={"k":3})
-
-prompt = PromptTemplate(
-    template='''You are a helpfull AI Assistant.
-    Answer the question from the following context. If Context if insufficient just say, I don't know.
-    {context}
-    Question:{question}
-    Answer:''',
-    input_variables=["context","question"]
-)
+from langchain_community.vectorstores import
 
 
+def my_docs(folder_path):
+    all_documents = []
+    for file in Path(folder_path).glob("*"):
+        if file.suffix == ".pdf":
+            loader = PyMuPDFLoader(str(file))
+        elif file.suffix == ".txt":
+            loader = TextLoader(str(file),encoding="utf-8")
+        elif file.suffix == ".docs":
+            loader = UnstructuredWordDocumentLoader(str(file))
+        else:
+            continue
 
-question = "what is sql"
-retrieve_docs = retriever.invoke(question)
+        documents = loader.load()
+        all_documents.extend(documents)
 
-context = " ".join([i.page_content for i in retrieve_docs])
+    return all_documents
 
-final_prompt = prompt.invoke({"context":context,"question":question})
+doc = my_docs("docs")
 
-result = llm.invoke(final_prompt)
-print(result.content)
+text_spiltter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=100)
+chunks = text_spiltter.split_documents(doc)
 
+print(len(chunks))
