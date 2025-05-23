28f41630a99319079328636a71a7c8fc58348568
bantee-sharma <banteesharma9@gmail.com>
2025-05-23 21:06:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from langchain_community.document_loaders import TextLoader,PyMuPDFLoader,Unstru
 from pathlib import Path
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import
+from langchain_community.vectorstores import FAISS
 
 
 def my_docs(folder_path):
